30a4e3f5781da3c6ec9d5799e2d8452d03bc55a9
athumani mwinami <mwinamijr@gmail.com>
2020-04-30 08:35:37 -0700

    created the tic tac toe board

---
+++ python_dictionary_tic_tac_toe.py
@@ -0,0 +1,6 @@
+#define the tic tac toe board using python dictionary
+
+theBoard = {'TL':' ', 'TM':' ', 'TR':' ',     #TL = TOP LEFT, TM = TOP MIDDLE , TR = TOP RIGHT
+			'ML':' ', 'MM':' ', 'MR':' ',     #ML = MIDDLE LEFT, MM = MIDDLE MIDDLE , MR = MIDDLE RIGHT
+			'LL':' ', 'LM':' ', 'LR':' '}     #LL = LOW LEFT, LM = LOW MIDDLE , LR = LOW RIGHT
+
